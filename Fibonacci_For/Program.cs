6c5596f3a0b4f79cc9e48bba02d67faa7a5257ae
unknown <80004643+GKavvadias@users.noreply.github.com>
2021-04-01 17:06:10 +0100

    Initial commit

---
+++ Fibonacci_For/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci_For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, F0 = 0, F1 = 1, F;
+            Console.Write("Παρακαλώ δώστε έναν αριθμό: ");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                F = F0 + F1;
+                Console.WriteLine(F);
+                F0 = F1;
+                F1 = F;
+            }
+        }
+    }
+}
